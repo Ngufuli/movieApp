c9f334e4e7f4ab797204fdb0404faf1241c6a57f
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-05-19 21:51:59 +0530

    our first web server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const port = 2222;
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/search', (req, res)=>{
+    res.render('search');
+});
+
+app.listen(port, ()=>{
+    console.log(`Server has started on port ${port}`);
+});
